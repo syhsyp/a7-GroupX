b580a6341a0dcc201f2c309ab3d9223a6373faa2
Yuhao <syh19961001@gmail.com>
2016-02-23 15:49:22 -0800

    summary

---
+++ scripts/summary.r
@@ -0,0 +1,14 @@
+library(dplyr)
+library(plotly)
+library(knitr)
+
+data <- read.csv('../data/intro_survey_data.csv')
+total_students <- nrow(data)
+
+data_by_class <- group_by(data, What.is.your.current.class.standing.) %>%
+                  summarise(total = n())
+most_class_standing <- as.character(arrange(data_by_class, desc(total))$What.is.your.current.class.standing.[1])
+
+num_apply_info <- nrow(filter(data, Are.you.interested.in.applying.to.the.Informatics.major. == "Yes"))
+
+num_country_more_than_ten <- nrow(filter(data, How.many.countries.have.you.visited.in.your.life. > 10))
