11ee13914a9636ea92f3d8d9550c3a148e679d44
Yuhao <syh19961001@gmail.com>
2016-02-23 20:20:01 -0800

    summary index.Rmd

--- scripts/chart1.r
+++ scripts/chart1.r
@@ -6,13 +6,16 @@ library(plotly)
 original <- read.csv("../data/intro_survey_data.csv")
 
 ## change the data to a more observed form
-survey <- group_by(original, What.is.your.current.class.standing.) %>% summarise(number = n())
+survey <- group_by(original, What.operating.system.do.you.typically.use.) %>% summarise(number = n())
 colnames(survey) <- c("Grade", "Number")
 
 ## plot the bar chart based on the grade and the students distrubution
-plot_ly(survey,
+build_chart1 <- function(my_data) {
+  chart1 <- plot_ly(my_data,
         x = Grade,
         y = Number,
         name = "Number",
         type = "bar"
-        ) %>% layout(title = "The Grade Distribution of INFO498F")
+        ) %>% layout(title = "The Class Standing Distribution of INFO498F")
+  return(chart1)
+}
