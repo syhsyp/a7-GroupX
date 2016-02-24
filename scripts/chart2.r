a59ae07fb95b0fba45333124d04451d21f62db54
gloriagou947 <gsq947@uw.edu>
2016-02-23 20:48:37 -0800

    changed chart2

--- scripts/chart2.r
+++ scripts/chart2.r
@@ -1,33 +1,26 @@
 # install and load packages
+<<<<<<< f0ae8e323edaa1abcd0d38159a150a6b065bd17b
+=======
+install.packages('plotrix')
+>>>>>>> changed chart2
 library(plotrix)
 library(dplyr)
-library(knitr)
 
 # Read in files
 data <- read.csv('../data/intro_survey_data.csv')
 
-# Write a function to find the total number of countries they visited for different class standing
-number_of_countries <- function(my_data){
-  outcome <- data %>% 
-   filter(What.is.your.current.class.standing. == my_data) %>%
-    select(How.many.countries.have.you.visited.in.your.life.)
-  return(sum(outcome$How.many.countries.have.you.visited.in.your.life.))
-}
-
-# Find the total number of countries vistied for Freshmen
-freshman <- number_of_countries('Freshman')
-
-# Find the total number of countries vistied for Sophomore
-sophomore <- number_of_countries('Sophomore')
-
-# Find the total number of countries vistied for Junior
-junior <- number_of_countries('Junior')
-
-# Find the total number of countries vistied for Senior
-senior <- number_of_countries('Senior')
+# Create a new dataset that contains the total number of countries they visited corresponding by
+# different class standing
+number_of_countries <- data %>%
+                         group_by(What.is.your.current.class.standing.) %>%
+                           select(How.many.countries.have.you.visited.in.your.life.) %>%
+                              summarise(total_countries = sum(How.many.countries.have.you.visited.in.your.life.))
 
 # Draw a pie chart that show total countried visited corresponding by different class standings
-slices <- c(freshman, sophomore, junior, senior) 
-lbls <- c('Freshman', 'Sophomore', 'Junior', 'Senior')
-pie3D(slices,labels=lbls,explode=0.1,
-      main="Pie Chart of Countries Visited")
+build_chart2 <- function(my_data){
+  slices <- my_data$total_countries 
+  lbls <- paste(my_data$What.is.your.current.class.standing., '\n', my_data$total_countries, sep = '')
+  chart2 <- pie3D(slices, labels=lbls, explode=0.1,
+                  main="Pie Chart of Countries Visited")
+  return(chart2)
+}
